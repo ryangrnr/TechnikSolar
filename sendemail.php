c91f792d99d37f951092ff54a7e291bf6c113d2f
Ryan Garner <Ryan Garner>
2015-11-16 22:58:19 +0000

    Added error logging to sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,17 +1,31 @@
 <?php
+ini_set("log_errors", 1);
+ini_set("error_log", "php-error.log");
+
 $name       = @trim(stripslashes($_POST['name']));
 $from       = @trim(stripslashes($_POST['email']));
 $subject    = @trim(stripslashes($_POST['subject']));
 $message    = @trim(stripslashes($_POST['message']));
-$to   		= 'ryangrnr@yahoo.co.uk';//replace with your email
+$to   		= 'ryalwyn@techniksolar.com';//replace with your email
+error_log("The email we are sending to is: ".$to);
+error_log("The name is: ".$_POST['name']);
+error_log("The from is:".$from);
+error_log("The subject is: ".$subject);
+error_log("The message is: ".$message);
 
-$headers = "MIME-Version: 1.0";
-$headers. = "Content-type: text/plain; charset=iso-8859-1";
-$headers. = "From: {$name} <{$from}>";
-$headers. = "Reply-To: <{$from}>";
-$headers. = "Subject: {$subject}";
-$headers. = "X-Mailer: PHP/".phpversion();
+$headers = "MIME-Version: 1.0\r\n";
+$headers.= "Content-type: text/plain; charset=iso-8859-1\r\n";
+$headers.= "From: {$name} <{$from}>\r\n";
+$headers.= "Reply-To: <{$from}>\r\n";
+$headers.= "X-Mailer: PHP/".phpversion()."\r\n";
+error_log("The headers are: ".$headers);
 
-mail($to, $subject, $message, $headers);
 
+$success = mail($to, $subject, $message, $headers);
+if(!$success) {
+  error_log("Recieved non success from mail");
+}
+else {
+  error_log("Recieved success from mail");
+}
 ?>
